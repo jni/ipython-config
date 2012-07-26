0d4440828a6c5afd2a19c10b6839bf5a8b673839
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2012-07-26 18:12:22 -0400

    Use IPYTHONDIR env var for install if available

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+if [ -n "${IPYTHONDIR+x}" ]; then
+    ipd=$IPYTHONDIR
+else ipd=~/.ipython
+fi
+
 if [ $# -eq 1 ]; then
     fn=$1
-else fn=~/.ipython/profile_default/ipython_config.py
+else fn=$ipd/profile_default/ipython_config.py
 fi
 
 if [ -f $fn ]; then
