1fc0c65f11f5d3578be091e3a968b4da45a8a01a
Juan Nunez-Iglesias <jni@janelia.hhmi.org>
2012-07-31 14:39:02 -0400

    Load Robert Kern's line_profiler magic by default

--- ipython_config.py
+++ ipython_config.py
@@ -90,7 +90,7 @@ c.TerminalIPythonApp.display_banner = False
 # c.TerminalIPythonApp.quick = False
 
 # A list of dotted module names of IPython extensions to load.
-# c.TerminalIPythonApp.extensions = []
+c.TerminalIPythonApp.extensions = ['line_profiler']
 
 # Whether to install the default config files into the profile dir. If a new
 # profile is being created, and IPython contains config files for that profile,
