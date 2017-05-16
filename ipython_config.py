47e333427ae2c98151e5666464c3dbddc44f5fe0
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2017-05-16 14:47:45 +1000

    Add mouse support to terminal prompt

--- ipython_config.py
+++ ipython_config.py
@@ -208,6 +208,9 @@ c.InteractiveShellApp.extensions = [
 # 
 # c.TerminalInteractiveShell.debug = False
 
+# Add mouse support
+c.TerminalInteractiveShell.mouse_support = True
+
 # Set to confirm when you try to exit IPython with an EOF (Control-D in Unix,
 # Control-Z/Enter in Windows). By typing 'exit' or 'quit', you can force a
 # direct exit without any confirmation.
