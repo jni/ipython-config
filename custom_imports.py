fdd6728417d110d484fd6d1fea5f925c6d571214
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2017-06-22 19:12:24 +1000

    Add more default imports

--- custom_imports.py
+++ custom_imports.py
@@ -1,6 +1,13 @@
-from skimage import *
+from skimage import io, filters, segmentation, morphology, exposure, draw
+from skimage import feature, future, geometry, transform, graph, util
+from skimage import measure, restoration, _shared
+import imageio as iio
+import sys
+import os
+import pdb
+import dask.array as da
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 import toolz as tz
-from scipy import sparse, ndimage as ndi
+from scipy import sparse, ndimage as ndi, optimize
